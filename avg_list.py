1bd6cc74a13b55125acf17f715c5571b9061b946
siddharthSpace <siddharthkesralikar1@gmail.com>
2026-09-15 11:37:12 +0530

    add thisfile

---
+++ avg_list.py
@@ -0,0 +1,6 @@
+l = [10, 20, 30, 40]
+
+total_sum = sum(l)
+count = len(l)
+
+print("The avg is:", total_sum/count)
---
+++ cal_sum.py
@@ -0,0 +1,9 @@
+
+l = [10, 20, 30, 40]
+
+
+total_sum = sum(l)
+
+print("The sum is:", total_sum)
+
+
